c6767a9acbca1a8891fffaf8eb1b662eaec2fb1e
Ha Dinh <hadinh893@gmail.com>
2023-06-06 20:35:09 +0700

    Reformat the code and remove absolute paths

--- database/database/polls/urls.py
+++ database/database/polls/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("data/", views.viewdata, name="data"),
--- database/database/polls/views.py
+++ database/database/polls/views.py
@@ -1,37 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import company,application
+from .models import company, application
 from .forms import PostProfile
 # Create your views here.
+
+
 def index(request):
-    return render(request,"polls/index.html")
+    return render(request, "polls/index.html")
+
 
 def viewdata(request):
     c = company.objects.all()
     context = {"ds": c}
-    return render(request,"polls/data.html",context)
+    return render(request, "polls/data.html", context)
 
-def detailview(request,company_id):
-    c = company.objects.get(pk = company_id)
-    return render(request,"polls/detail.html",{"com": c})
+
+def detailview(request, company_id):
+    c = company.objects.get(pk=company_id)
+    return render(request, "polls/detail.html", {"com": c})
+
 
 def login(request):
-    return render(request,"polls/login.html")
+    return render(request, "polls/login.html")
+
 
 def forgot_password(request):
-    return render(request,"polls/forgot-password.html")
+    return render(request, "polls/forgot-password.html")
+
 
 def register(request):
-    return render(request,"polls/register.html")
+    return render(request, "polls/register.html")
+
 
 def hisview(request):
     c = application.objects.all()
-    context = {"app":c}
-    return render(request,"polls/history_view.html",context)
+    context = {"app": c}
+    return render(request, "polls/history_view.html", context)
+
 
 def myresume(request):
     a = PostProfile()
-    return render(request,"polls/resume.html",{'f': a})
+    return render(request, "polls/resume.html", {'f': a})
+
 
 def save_profile(request):
     if request.method == "POST":
@@ -42,5 +52,4 @@ def save_profile(request):
         else:
             return HttpResponse("khong luu duoc")
     else:
-        return HttpResponse("khong phai post request")    
-    
+        return HttpResponse("khong phai post request")
