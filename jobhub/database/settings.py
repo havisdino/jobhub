fd76f8ec2e983648ed3aea63137ab656f3bb971a
Ha Dinh <hadinh893@gmail.com>
2023-07-13 23:16:49 +0700

    Update settings.py

--- jobhub/database/settings.py
+++ jobhub/database/settings.py
@@ -29,7 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ.keys()
+# DEBUG = 'RENDER' not in os.environ.keys()
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -137,10 +138,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = "static/"
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "polls/static"),
-# )
-if not DEBUG:
+# if not DEBUG:
+if True: # Fix this later
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
